c59d690945f8cbd6d2e3932e1b42910c51fb5d3c
Leo CAI <@12104377>
2025-08-26 01:58:40 +0200

    [TR] Implémentation TextRank (damping=0.85, convergence tolérance=1e-6)

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from utils import lire_fichier,lire_fichiers
 from preprocessing import nettoyer_segmenter
 from tfidf import construire_tfidf
 from graph import similarity_matrix
+from textrank import textrank
+import networkx as nx
 
 texte_brute = lire_fichier("exemples/article1.txt")
 list_phrases = nettoyer_segmenter(texte_brute, langue="french")      #'french' ou 'english'
@@ -16,12 +18,18 @@ print(list_phrases)
 
 #tfidf.py
 matrice, vocab = construire_tfidf(list_phrases)
-print("Vocabulaire:", vocab)
-print("\nMatrice TF-IDF:" ,matrice)
+# print("Vocabulaire:", vocab)
+# print("\nMatrice TF-IDF:" ,matrice)
 
 #graph.py
-G = similarity_matrix(matrice, seuil=0.1)
-print("Nœuds du graphe :", G.nodes())
-print("Arêtes du graphe :", G.edges())
-print("Arêtes du graphe avec poids :", G.edges(data=True))
+G = similarity_matrix(matrice, seuil=0.01)
+# print("Nœuds du graphe :", G.nodes())
+# print("Arêtes du graphe :", G.edges())
+# print("Arêtes du graphe avec poids :", G.edges(data=True))
 
+# conversion graphe -> matrice d'adjacence
+A = nx.to_numpy_array(G, weight="weight")
+
+#textrank.py
+scores = textrank(A)
+print("Scores finaux:", scores)
---
+++ textrank.py
@@ -0,0 +1,54 @@
+# textrank.py
+import numpy as np
+
+def textrank(adj_matrix, damping=0.85, max_iter=100, tol=1e-6):
+    
+    n = len(adj_matrix)
+    
+    if n == 0:
+        return []
+
+    # convertir en numpy array
+    A = np.array(adj_matrix, dtype=float)
+
+    # cas sans arêtes
+    if np.all(A == 0):
+        return [1.0 / n] * n
+    
+    # normalisation : axis=1 (lignes), keepdims=True (keep dimension)
+    row_sums = A.sum(axis=1, keepdims=True)
+
+    M = np.zeros_like(A)
+    for i in range(n):  # pour chaque phrase (ligne)
+        if row_sums[i] != 0:  # eviter les divisions par zero
+            M[i] = A[i] / row_sums[i]
+
+    # initialisation : score uniforme 1/n
+    scores = np.ones(n) / n
+
+
+    for iter in range(max_iter):
+        new_scores = np.zeros(n)
+        
+        for i in range(n):
+            # random page is 1 - d
+            new_scores[i] = (1 - damping) / n
+            
+            # contribution des voisins (redistribution des scores)
+            for j in range(n):
+                new_scores[i] += damping * M[j, i] * scores[j]
+        
+        # Vérifier convergence (écart entre anciens et nouveaux scores)
+        diff = np.sum(np.abs(new_scores - scores))
+        if diff < tol:
+            print(f"\nConvergence en {iter} itérations\n")
+            break
+
+        scores = new_scores
+
+    # Normalisation entre 0 et 1
+    max_score = max(scores)
+    if max_score > 0:
+        scores = scores / max_score
+
+    return scores.tolist()
